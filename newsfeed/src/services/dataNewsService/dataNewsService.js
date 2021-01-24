c69d6203ec5fb4bc0b22c6145be47cf6d3609b06
Fion Wan <fion.wan8@gmail.com>
2021-01-24 16:36:41 -0700

    Headlines rendered

--- newsfeed/src/services/dataNewsService/dataNewsService.js
+++ newsfeed/src/services/dataNewsService/dataNewsService.js
@@ -1,37 +1,38 @@
-import config from 'config';
 import { get } from 'lodash';
 import axios from 'axios';
 import { dataNewsServiceOperationType } from './dataNewsServiceOperationType';
 
-const dataNewsProviderConfig = config.get('dataNewsProviderConfig');
-console.log(dataNewsProviderConfig);
+const dataNewsProviderConfig = {
+    auth: {
+        apiKey: '09wicv2sjsax89zuxnz23enzr',
+    },
+    api: {
+        port: 443,
+        protocol: 'http',
+        host: 'api.datanews.io/v1',
+        [dataNewsServiceOperationType.GET_HEADLINES]: {
+            verb: 'GET',
+            endpoint: '/headlines',
+        }
+
+    }
+};
 
 const getDataNewsApiProperties = (dataNewsProviderConfig) => {
-    const method = get(dataNewsProviderConfig, `${dataNewsServiceOperationType.GET_HEADLINES}.verb`)
-    const protocol = get(dataNewsProviderConfig, 'api.protocol');
     const host = get(dataNewsProviderConfig, 'api.host'); 
-    const endpoint = get(dataNewsProviderConfig, `${dataNewsServiceOperationType.GET_HEADLINES}.endpoint`)
+    const endpoint = get(dataNewsProviderConfig, `api.${dataNewsServiceOperationType.GET_HEADLINES}.endpoint`)
     const auth = get(dataNewsProviderConfig, 'auth.apiKey');
+    const url = `${host}${endpoint}`;
     
-    const url = `${protocol}${host}${endpoint}`;
     return {
-        url,
-        method,
+        url, 
         auth,
     }
 }
 
-
-export const getHeadlinesFromDataNewsApi = (getDataNewsApiProperties) => {
-    const url = get(getDataNewsApiProperties, 'url');
-    const method = get(getDataNewsApiProperties, 'method');
-    
-    axios({
-        method,
-        url,
-        headers: {
-            'api-key': auth
-            }
-    })
-    .then(response => console.log('response', response));
+export const getHeadlinesFromDataNewsApi = () => {
+    const { url, auth } = getDataNewsApiProperties(dataNewsProviderConfig) || {};
+    return axios.get(`http://${url}?apiKey=${auth}`)
+        .then(response => response)
+        .catch(error => console.log(error));
 };
--- newsfeed/src/views/home/Home.jsx
+++ newsfeed/src/views/home/Home.jsx
@@ -1,65 +1,47 @@
 import { React, Component } from 'react';
+import { get } from 'lodash';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
-import { getHeadlinesFromDataNewsApi } from '../../services/dataNewsService';
+import { getHeadlinesFromDataNewsApi } from '../../services/dataNewsService/dataNewsService';
+
 
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = []
+        this.state = {
+            headlines: []
+        }
+    };
+
+    setHeadlinesToState() {
+       return getHeadlinesFromDataNewsApi()
+        .then(response => {
+            const headlines = get(response, 'data.hits')
+            this.setState({
+                headlines,
+            })
+        });
     }
 
     componentDidMount() {
-        console.log(getHeadlinesFromDataNewsApi);
-        //.then(response => {this.setState = response.data.hits})
+        this.setHeadlinesToState()
     }
-
+    
     render() {
-        const useStyles = makeStyles((theme) => ({
-            formControl: {
-            margin: theme.spacing(1),
-            minWidth: 120,
-            },
-            selectEmpty: {
-            marginTop: theme.spacing(2),
-            },
-        }));
+        const headlinesList = this.state.headlines;
 
-        function SimpleSelect() {
-            const classes = useStyles();
-            const [age, setAge] = React.useState('');
-        
-            const handleChange = (event) => {
-            setAge(event.target.value);
-            };
-
-        };
-        
-        // classes.formControl
+       
       return (
         <div>
           <h1>Newsfeed</h1>
-          <FormControl className={null}> 
-            <InputLabel id="demo-simple-select-helper-label">News Headlines</InputLabel>
-            <Select
-            labelId="demo-simple-select-helper-label"
-            id="demo-simple-select-helper"
-            value={2} //age
-            onChange={null} //handlechange
-            >
-            <MenuItem value="">
-                <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-            </Select>
-            <FormHelperText>Select an article</FormHelperText>
-        </FormControl>
+          <h2>Top Headlines</h2>
+          {headlinesList.map((headline) => (
+            <h2>{headline.title}</h2>
+            ))}
         </div>
       );
     }
